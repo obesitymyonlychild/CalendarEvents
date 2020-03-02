164425897a0ab8df4ee088c28da6696ce60a676a
Fei Wu <feii.wu@mail.utoronto.ca>
2020-03-02 18:49:02 -0500

    User add getEvents()

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class User implements Serializable {
     private String name;
     private String password;
-    private ArrayList<Event> pastEvent;
-    private ArrayList<Event> futureEvent;
-    private ArrayList<Event> ongoingEvent;
-    private ArrayList<Memo> memo;
+    private ArrayList<Event> pastEvents;
+    private ArrayList<Event> futureEvents;
+    private ArrayList<Event> ongoingEvents;
+    private ArrayList<Memo> memos;
     boolean alertOn;
     private ArrayList<Alert> alertList;
 
@@ -32,29 +32,38 @@ public class User implements Serializable {
         this.password = psw;
     }
 
-    public ArrayList<Event> getPastEvent() {
-        return pastEvent;
+    public ArrayList<Event> getPastEvents() {
+        return pastEvents;
     }
 
-    public void setPastEvent(ArrayList<Event> pastEvent) {
-        this.pastEvent = pastEvent;
+    public void setPastEvents(ArrayList<Event> pastEvents) {
+        this.pastEvents = pastEvents;
     }
 
-    public ArrayList<Event> getFutureEvent() {
-        return futureEvent;
+    public ArrayList<Event> getFutureEvents() {
+        return futureEvents;
     }
 
-    public void setFutureEvent(ArrayList<Event> futureEvent) {
-        this.futureEvent = futureEvent;
+    public void setFutureEvents(ArrayList<Event> futureEvents) {
+        this.futureEvents = futureEvents;
     }
 
-    public ArrayList<Event> getOngoingEvent() {
-        return ongoingEvent;
+    public ArrayList<Event> getOngoingEvents() {
+        return ongoingEvents;
     }
 
-    public void setOngoingEvent(ArrayList<Event> ongoingEvent) {
-        this.ongoingEvent = ongoingEvent;
+    public ArrayList<Event> getEvents(){
+        ArrayList<Event> all = new ArrayList<>();
+        all.addAll(this.pastEvents);
+        all.addAll(this.ongoingEvents);
+        all.addAll(this.futureEvents);
+        return all;
     }
+
+    public void setOngoingEvent(ArrayList<Event> ongoingEvents) {
+        this.ongoingEvents = ongoingEvents;
+    }
+
 
     public ArrayList<Alert> getAlertList() {
         return alertList;
@@ -64,25 +73,25 @@ public class User implements Serializable {
         this.alertList = alertList;
     }
 
-    public ArrayList<Memo> getMemo() {
-        return memo;
+    public ArrayList<Memo> getMemos() {
+        return memos;
     }
 
     public void setMemo(ArrayList<Memo> mem) {
-        this.memo = mem;
+        this.memos = mem;
     }
 
     public Memo createMemo(String name, String content){
         Memo mm = new Memo(name, content);
-        this.memo.add(mm);
+        this.memos.add(mm);
         return mm;
     }
 
-    public void deleteMemo(String name){
+    public void deleteMemos(String name){
         boolean res = false;
-        for (int i=0; i < this.memo.size(); i++){
-            if(this.memo.get(i).getName().equals(name)){
-                this.memo.remove(i);
+        for (int i=0; i < this.memos.size(); i++){
+            if(this.memos.get(i).getName().equals(name)){
+                this.memos.remove(i);
                 res = true;
                 break;
             }
@@ -92,45 +101,45 @@ public class User implements Serializable {
         }
     }
 
-    public Event createEvent(String name, String startTime, long duration, String address){
+    public void createEvent(String name, String startTime, long duration, String address){
         Event evt = new Event(name, startTime, duration, address);
         LocalDateTime start = evt.getStartTime();
         LocalDateTime end = start.plusMinutes(duration);
         LocalDateTime now = LocalDateTime.now();
         if(start.isAfter(now)){
-            this.futureEvent.add(evt);
+            this.futureEvents.add(evt);
         }
         else if(start.isBefore(now)){
             if (end.isBefore(now)){
-                this.pastEvent.add(evt);
+                this.pastEvents.add(evt);
             }
-            else{this.ongoingEvent.add(evt);
+            else{this.ongoingEvents.add(evt);
             }
         }
         else{
-            this.ongoingEvent.add(evt);
+            this.ongoingEvents.add(evt);
         }
     }
 
     public void deleteEvent(String name){
         boolean res = false;
-        for (int i=0; i < this.ongoingEvent.size(); i++){
-            if (this.ongoingEvent.get(i).getName().equals(name)){
-                this.ongoingEvent.remove(i);
+        for (int i=0; i < this.ongoingEvents.size(); i++){
+            if (this.ongoingEvents.get(i).getName().equals(name)){
+                this.ongoingEvents.remove(i);
                 res = true;
                 break;
             }
         }
-        for (int i=0; i < this.pastEvent.size(); i++){
-            if (this.pastEvent.get(i).getName().equals(name)){
-                this.pastEvent.remove(i);
+        for (int i=0; i < this.pastEvents.size(); i++){
+            if (this.pastEvents.get(i).getName().equals(name)){
+                this.pastEvents.remove(i);
                 res = true;
                 break;
             }
         }
-        for (int i=0; i < this.futureEvent.size(); i++){
-            if (this.futureEvent.get(i).getName().equals(name)){
-                this.futureEvent.remove(i);
+        for (int i=0; i < this.futureEvents.size(); i++){
+            if (this.futureEvents.get(i).getName().equals(name)){
+                this.futureEvents.remove(i);
                 res = true;
                 break;
             }
