c49ccfcb5fa845b83d5be90bcb93cd86d49e3361
Fei Wu <feii.wu@mail.utoronto.ca>
2020-03-02 19:50:51 -0500

    User add series and searchEventsBySeries

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,3 +1,4 @@
+import java.io.EOFException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -10,6 +11,7 @@ public class User implements Serializable {
     private ArrayList<Memo> memos;
     boolean alertOn;
     private ArrayList<Alert> alertList;
+    private ArrayList<Series> series;
 
     public User(String name, String psw){
         this.name = name;
@@ -52,6 +54,10 @@ public class User implements Serializable {
         return ongoingEvents;
     }
 
+    public void setOngoingEvent(ArrayList<Event> ongoingEvents) {
+        this.ongoingEvents = ongoingEvents;
+    }
+
     public ArrayList<Event> getEvents(){
         ArrayList<Event> all = new ArrayList<>();
         all.addAll(this.pastEvents);
@@ -59,11 +65,6 @@ public class User implements Serializable {
         all.addAll(this.futureEvents);
         return all;
     }
-
-    public void setOngoingEvent(ArrayList<Event> ongoingEvents) {
-        this.ongoingEvents = ongoingEvents;
-    }
-
 
     public ArrayList<Alert> getAlertList() {
         return alertList;
@@ -81,10 +82,36 @@ public class User implements Serializable {
         this.memos = mem;
     }
 
-    public Memo createMemo(String name, String content){
+    public ArrayList<Series> getSeries(){
+        return this.series;
+    }
+
+    public void setSeries(ArrayList<Series> series){
+        this.series = series;
+    }
+
+    public ArrayList<Event> getOrder(ArrayList<Event> events){
+        ArrayList<Event> res = new ArrayList<>();
+        for (Event event : events) {
+            if (res.size() == 0) {
+                res.add(events.get(0));
+            } else {
+                for (int s = 0; s < res.size(); s++) {
+                    if (event.getStartTime().isBefore(res.get(s).getStartTime())) {
+                        res.add(s, event);
+                    }
+                }
+                if (!res.contains(event)) {
+                    res.add(event);
+                }
+            }
+        }
+        return res;
+    }
+
+    public void createMemo(String name, String content){
         Memo mm = new Memo(name, content);
         this.memos.add(mm);
-        return mm;
     }
 
     public void deleteMemos(String name){
@@ -147,6 +174,20 @@ public class User implements Serializable {
         if (!res){
             System.out.println("No such event exists!");
         }
+    }
+
+    public void createSeries(String name){
+        Series sir  = new Series(name);
+        this.series.add(sir);
+    }
+
+    public ArrayList<Event> searchEventsBySeries(String name){
+        for(Series series: this.series){
+            if (series.getName().equals(name)){
+                return series.getEvents();
+            };
+        }
+        System.out.println("Series does not exists!");
     }
 
 }
