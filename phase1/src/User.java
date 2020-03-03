a07e97e298e2a24eae965b3edd0627407a92d790
Fei Wu <feii.wu@mail.utoronto.ca>
2020-03-02 22:21:19 -0500

    boolean type constrained

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -10,7 +10,7 @@ public class User implements Serializable{
     private ArrayList<Event> ongoingEvents;
     private ArrayList<Event> events;
     private ArrayList<Memo> memos;
-    boolean alertOn;
+    private boolean alertOn;
     private ArrayList<Alert> alertList;
     private ArrayList<Series> series;
 
