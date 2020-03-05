27aeb632f9a22034bf267367ad40356579aed26c
qianding <bruce,qian@mail.utoronto.ca>
2020-03-04 21:49:40 -0500

    modify main

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     static ArrayList<String> Users = new ArrayList<String>();
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         File file = new File("ProjectGroups//Users");
-        if (file.exists()){
+        if (!file.exists()){
         ObjectOutputStream users = new ObjectOutputStream(new FileOutputStream("Users"));
         users.writeObject(Users);
         users.close();}
