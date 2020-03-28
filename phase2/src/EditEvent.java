f2e01f30f4432f11476a54766b4e0ac508ea7363
Fei Wu <feii.wu@mail.utoronto.ca>
2020-03-27 20:54:49 -0400

    changes in AddEventFrame

--- phase2/src/EditEvent.java
+++ phase2/src/EditEvent.java
@@ -8,21 +8,14 @@ import java.util.ArrayList;
 public class EditEvent extends JFrame implements ActionListener {
 
     Container container = getContentPane();
-    JLabel nameLabel = new JLabel("EVENT NAME");
-    JLabel dateLabel = new JLabel("START TIME");
-    JLabel durationLabel = new JLabel("DURATION");
-    JLabel formatLabel = new JLabel("YYYY-MM-DD HH:MM");
-    JLabel inMinuteLabel = new JLabel("In Minutes");
-    JLabel addressLabel = new JLabel("ADDRESS");
-    JTextField nameTextField = new JTextField();
-    JTextField dateTextField = new JTextField();
-    JTextField durationTextField = new JTextField();
-    JTextField addressTextField = new JTextField();
-    JButton addButton = new JButton("ADD");
-    JButton backButton = new JButton("BACK");
 
 
     EditEvent() {
+        this.setTitle("Edit Event");
+        this.setVisible(true);
+        this.setBounds(10, 10, 500, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         setLayoutManager();
         setLocationAndSize();
         addComponentsToContainer();
@@ -35,42 +28,17 @@ public class EditEvent extends JFrame implements ActionListener {
     }
 
     public void setLocationAndSize() {
-        nameLabel.setBounds(50, 150, 100, 30);
-        dateLabel.setBounds(50, 220, 100, 30);
-        nameTextField.setBounds(150, 150, 150, 30);
-        dateTextField.setBounds(150, 220, 150, 30);
-        formatLabel.setBounds(320, 220, 200, 30);
-        durationLabel.setBounds(50, 290, 100, 30);
-        inMinuteLabel.setBounds(320, 290, 200, 30);
-        durationTextField.setBounds(150, 290, 150, 30);
-        addressLabel.setBounds(50, 360, 100, 30);
-        addressTextField.setBounds(150, 360, 150, 30);
-        addButton.setBounds(300, 500, 100, 30);
-        backButton.setBounds(50, 500, 100, 30);
 
 
     }
 
     public void addComponentsToContainer() {
-        container.add(nameLabel);
-        container.add(durationLabel);
-        container.add(dateLabel);
-        container.add(formatLabel);
-        container.add(addressLabel);
-        container.add(addButton);
-        container.add(backButton);
-        container.add(nameTextField);
-        container.add(addressTextField);
-        container.add(durationTextField);
-        container.add(dateTextField);
-        container.add(inMinuteLabel);
 
 
     }
 
     public void addActionEvent() {
-        addButton.addActionListener(this);
-        backButton.addActionListener(this);
+
     }
 
     static ArrayList<String> Users = new ArrayList<String>();
@@ -79,24 +47,14 @@ public class EditEvent extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         //Coding part of go back button
-        if (e.getSource() == backButton){
-            this.dispose();
+        //if (e.getSource() == backButton){
+            //this.dispose();
 
 
         }
 
-        if(e.getSource() == addButton){
-            String name = nameTextField.getText();
-            String startTime = dateTextField.getText();
-            int duration = Integer.parseInt(durationTextField.getText());
-            String address = addressTextField.getText();
-            CalendarFacade.getCurrentUser().createEvent(name, startTime, duration, address);
-            JOptionPane.showMessageDialog(this, "Event added!");
-            nameTextField.setText("");
-            dateTextField.setText("");
-            durationTextField.setText("");
-            addressTextField.setText("");
-        }
-    }
+        //if(e.getSource() == addButton){
+        
+
 }
 
